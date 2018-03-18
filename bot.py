5b462ed984ab2ca509568c9142535fe35140d48a
Roman Isko <iskoroman@gmail.com>
2018-03-18 19:11:32 +0200

    added help message with image

--- bot.py
+++ bot.py
@@ -21,8 +21,10 @@ def start(bot, update):
 
 
 def help(bot, update):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
+    update.message.reply_photo(
+        photo='https://imagecdn1.luxnet.ua/zaxid/resources/photos/news/500_DIR/201702/1418712_1458359.jpg',
+        caption='Для отримання інформації, потрібно відправити номер зупинки і я постараюсь знайти інформацію '
+                'по громадському транспорту для цієї зупинки')
 
 
 def error(bot, update, error):
