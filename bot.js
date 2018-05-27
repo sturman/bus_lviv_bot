b61ea221e89129de36daa3f4ff9a603e555c66c1
Roman Isko <iskoroman@gmail.com>
2018-05-27 22:10:25 +0300

    extract help text into const variable

--- bot.js
+++ bot.js
@@ -11,12 +11,14 @@ bot.telegram.getMe().then((botInfo) => {
 const startText =
   'Вітаю! Я допоможу Вам знайти розклад громадського транспорту Львова, якщо ви відправите мені номер зупинки. \n ' +
   'Наприклад, 216'
+const helpText = 'Для отримання інформації, потрібно відправити номер зупинки і ' +
+  'я постараюсь знайти інформацію по громадському транспорту для цієї зупинки'
 
 bot.start((ctx) => ctx.reply(startText))
-bot.help((ctx) => ctx.replyWithPhoto('https://imagecdn1.luxnet.ua/zaxid/resources/photos/news/500_DIR/201702/1418712_1458359.jpg',
-  {
-    caption: 'Для отримання інформації, потрібно відправити номер зупинки і я постараюсь знайти інформацію по громадському транспорту для цієї зупинки'
-  }))
+bot.help((ctx) => ctx.replyWithPhoto(
+  'https://imagecdn1.luxnet.ua/zaxid/resources/photos/news/500_DIR/201702/1418712_1458359.jpg',
+  {caption: helpText}
+))
 
 bot.hears(/\d/, (ctx) => {
   let busStopId = ctx.message.text
