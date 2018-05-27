2ceeb93131b33e492b175cfe91e5c67d10660d42
Roman Isko <iskoroman@gmail.com>
2018-05-27 22:07:55 +0300

    refactor method names

--- bot.js
+++ bot.js
@@ -22,14 +22,15 @@ bot.hears(/\d/, (ctx) => {
   let busStopId = ctx.message.text
   rp(`https://lad.lviv.ua/api/stops/${busStopId}`)
     .then(function (resp) {
-      return ctx.replyWithMarkdown(parseAPIResponse(busStopId, resp))
+      return ctx.replyWithMarkdown(prepareResponse(busStopId, resp))
     })
     .catch(function (err) {
       return ctx.reply(`Упс. Щось поламалось. Спробуйте пізніше\n----------\n${err}`)
     })
 })
 
-function parseAPIResponse (busStopId, resp) {
+// parse and transform API response
+function prepareResponse (busStopId, resp) {
   try {
     resp = JSON.parse(resp)
   }
@@ -39,14 +40,14 @@ function parseAPIResponse (busStopId, resp) {
   let replyMarkdown = ''
   let header = `Інформація по зупинці \n*${busStopId}* \`"${resp.name}"\`\n------------------------------\n`
   let routes = resp.timetable
-  let busInfo = parseRoutesInfo(routes)
+  let busInfo = parseBusInfo(routes)
 
   replyMarkdown += header
   replyMarkdown += busInfo
   return replyMarkdown
 }
 
-function parseRoutesInfo (routes) {
+function parseBusInfo (routes) {
   let busInfo = ''
   for (let route of routes) {
     busInfo += `${convertVehicleTypeToEmoji(route.vehicle_type)} ${route.route} - ${route.time_left} \n`
