5fe4a7f94aa480309963426878e471b621cdef66
Roman Isko <iskoroman@gmail.com>
2018-05-28 11:22:35 +0300

    Revert "Logzio logging"

--- bot.js
+++ bot.js
@@ -2,20 +2,7 @@ const Telegraf = require('telegraf')
 const rp = require('request-promise')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
-
-const logger = require('logzio-nodejs').createLogger({
-  token: process.env.LOGZIO_TOKEN,
-  host: 'listener.logz.io',
-  type: process.env.NODE_ENV === ('prod' || 'production') ? 'bus_lviv_bot' : 'bus_lviv_bot_dev'
-})
-
-//register logz.io and console loggers
-bot.use((ctx, next) => {
-  return next(ctx).then(() => {
-    console.log(ctx.message)
-    logger.log(ctx.message)
-  })
-})
+bot.use(Telegraf.log())
 
 bot.telegram.getMe().then((botInfo) => {
   bot.options.username = botInfo.username
