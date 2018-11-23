524ebb64e8b50e70c617139630512bcbe8c824c3
Roman Isko <iskoroman@gmail.com>
2018-11-23 10:22:00 +0200

    return promise

--- bot.js
+++ bot.js
@@ -50,7 +50,7 @@ bot.start((ctx) => {
 })
 bot.help((ctx) => ctx
   .replyWithPhoto('https://imagecdn1.luxnet.ua/zaxid/resources/photos/news/500_DIR/201702/1418712_1458359.jpg')
-  .then(ctx.replyWithMarkdown(helpText))
+  .then(() => {return ctx.replyWithMarkdown(helpText)})
 )
 
 bot.hears(/(^\d+$)|(^\/\d+$)/, (ctx) => {
