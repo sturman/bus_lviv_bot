222cea0a0b8dd63e0fbb917dbe8fb59735ed47b2
Roman Isko <iskoroman@gmail.com>
2019-07-14 23:17:24 +0300

    Implement mechanism to found the closest stops on location message

--- serverless_bot.js
+++ serverless_bot.js
@@ -1,5 +1,11 @@
 const Telegraf = require('telegraf')
+const Extra = require('telegraf/extra')
+const Markup = require('telegraf/markup')
+const rp = require('request-promise')
+
 const bot = new Telegraf(process.env.BOT_TOKEN)
+const apiLogin = process.env.API_LOGIN
+const apiPass = process.env.API_PASSWORD
 
 const startText =
   'Вітаю! Я допоможу Вам знайти розклад громадського транспорту Львова, якщо ви відправите мені номер зупинки. \n ' +
@@ -13,6 +19,29 @@ bot.help((ctx) => ctx
   .replyWithPhoto('https://imagecdn1.luxnet.ua/zaxid/resources/photos/news/500_DIR/201702/1418712_1458359.jpg')
   .then(() => ctx.replyWithMarkdown(helpText))
 )
+bot.on('location', (ctx) => {
+  let requestOptions = {
+    uri: 'https://api.eway.in.ua/',
+    qs: {
+      login: apiLogin,
+      password: apiPass,
+      city: 'lviv',
+      function: 'stops.GetStopsNearPoint',
+      lat: ctx.message.location.latitude,
+      lng: ctx.message.location.longitude
+    },
+    json: true
+  }
+
+  rp(requestOptions)
+    .then(closestStopsRes => {
+      let closestStopsKeyboard = []
+      closestStopsRes.stop.forEach(stop => {
+        closestStopsKeyboard.push(Markup.callbackButton(stop.title, stop.id))
+      })
+      ctx.reply('Найближчі зупинки:', Extra.markup((m) => m.inlineKeyboard(closestStopsKeyboard, { wrap: () => true })))
+    })
+})
 
 exports.handler = (event, context, callback) => {
   const body = JSON.parse(event.body) // get data passed to us
