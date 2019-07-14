7d930c3f23700fdee20ae54e1809d358d9d38bc8
Roman Isko <iskoroman@gmail.com>
2019-07-14 23:02:51 +0300

    Implement /start and /help commands for serverless bot

--- serverless_bot.js
+++ serverless_bot.js
@@ -1,15 +1,27 @@
-'use strict'
-
 const Telegraf = require('telegraf')
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-bot.hears('hi', ctx => ctx.reply('Hey there!'))
+const startText =
+  'Вітаю! Я допоможу Вам знайти розклад громадського транспорту Львова, якщо ви відправите мені номер зупинки. \n ' +
+  'Наприклад, 216'
+const helpText = `Для отримання інформації, потрібно відправити номер зупинки і я постараюсь знайти інформацію по громадському транспорту для цієї зупинки.
 
-/* AWS Lambda handler function */
+Також я вмію шукати найближчі зупинки. Для цього просто відправ мені свою локацію \u{1F4CE} `
+
+bot.start((ctx) => ctx.reply(startText))
+bot.help((ctx) => ctx
+  .replyWithPhoto('https://imagecdn1.luxnet.ua/zaxid/resources/photos/news/500_DIR/201702/1418712_1458359.jpg')
+  .then(() => ctx.replyWithMarkdown(helpText))
+)
+
 exports.handler = (event, context, callback) => {
-  const tmp = JSON.parse(event.body) // get data passed to us
-  bot.handleUpdate(tmp) // make Telegraf process that data
-  return callback(null, { // return something for webhook, so it doesn't try to send same stuff again
+  const body = JSON.parse(event.body) // get data passed to us
+  console.log('--------------')
+  console.log(body)
+  console.log('--------------')
+
+  bot.handleUpdate(body)
+  return callback(null, {
     statusCode: 200,
     body: '',
   })
