2505bb71139741314876b0c3fc4a1b0e77e530f1
Roman Isko <iskoroman@gmail.com>
2019-11-08 15:11:58 +0200

    Include bus stop id in response so user can easier re-request info

--- bot.js
+++ bot.js
@@ -32,7 +32,7 @@ bot.hears(/(^\d+$)|(^\/\d+$)/, (ctx) => {
     headers: { 'referer': `https://lad.lviv.ua/api/stops/${busStopId}` }
   })
     .then(resp => {
-      ctx.replyWithMarkdown(prepareResponse(busStopId, resp), Extra.inReplyTo(ctx.message.message_id))
+      ctx.replyWithMarkdown(prepareResponse(busStopId, resp) + `\n/${busStopId}`, Extra.inReplyTo(ctx.message.message_id))
     })
     .catch(err => {
       ctx.reply(`Упс. Щось поламалось. Отримано помилку від джерела даних\n----------\n${err}`, Extra.inReplyTo(ctx.message.message_id))
