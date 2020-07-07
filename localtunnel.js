9648fda7bcb1573be9bc5c90a12050a1643f65a7
sturman <iskoroman@gmail.com>
2020-07-07 23:43:35 +0300

    Use own localtunnel instance

--- localtunnel.js
+++ localtunnel.js
@@ -1,9 +1,9 @@
 const localtunnel = require('localtunnel');
 
 (async () => {
-  const tunnel = await localtunnel({ port: 3000 })
+  const tunnel = await localtunnel({ port: 3000, host: 'https://sturman.pp.ua' })
 
-  console.log(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/setWebhook?url=${tunnel.url}/bot`)
+  console.log(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/setWebhook?url=${tunnel.url}/development/bot`)
 
   tunnel.on('close', () => {
     // tunnels are closed
