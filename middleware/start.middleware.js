8f734246e77cc6df103565c42a519519b4fe9f5f
sturman <4456572+sturman@users.noreply.github.com>
2021-02-04 20:46:01 +0200

    Send photo on start command

--- middleware/start.middleware.js
+++ middleware/start.middleware.js
@@ -2,11 +2,12 @@ const Markup = require('telegraf/markup')
 const Extra = require('telegraf/extra')
 
 const startText =
-  'Вітаю! Я допоможу Вам знайти розклад громадського транспорту Львова, якщо ви відправите мені номер зупинки, наприклад 216, або свою локацію і я знайду найближчі зупинки'
+  'Вітаю! Я допоможу Вам знайти розклад громадського транспорту Львова, якщо ви відправите мені номер зупинки (виділено червоним на фото), наприклад 216, або свою локацію і я знайду найближчі зупинки'
 
 module.exports = (ctx, next) => {
   const keyboard = Markup.resize().keyboard([
     Markup.locationRequestButton('\u{1F4CC} Відправити локацію')
   ])
-  ctx.reply(startText, Extra.markup(keyboard).markdown())
+  ctx.replyWithPhoto('https://imagecdn1.luxnet.ua/zaxid/resources/photos/news/500_DIR/201702/1418712_1458359.jpg')
+    .then(() => ctx.reply(startText, Extra.markup(keyboard).markdown()))
 }
